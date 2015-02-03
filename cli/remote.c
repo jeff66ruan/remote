43f348098edc5e1ed277e6deebe9aa2c57bfe3a8
jeff66ruan <jeff66ruan@gmail.com>
2015-02-03 16:24:54 +1300

    application empty implmentation

---
+++ cli/remote.c
@@ -0,0 +1,18 @@
+/**
+ * Copyright:
+ *
+ * This demos a 2 button remote control for a TV
+ *
+ * */
+
+#include <stdio.h>
+
+int main()
+{
+  /* at the beginning I use four buttons in order to finish the whole
+     application framework. Changing back to two buttons shouldn't
+     affect underlining structure except the way of interpreting
+     button behaviours'*/
+  printf("start here\n");
+  return 0;
+}
